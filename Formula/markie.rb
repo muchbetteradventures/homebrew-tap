360bcf188816bafdd0678cb4cb96ee609c40a106
Andy Bennett <andy@muchbetteradventures.com>
2026-03-03 15:41:09 +0000

    0.0.2

--- Formula/markie.rb
+++ Formula/markie.rb
@@ -4,9 +4,9 @@ class Markie < Formula
   license "MIT"
 
   # Update these on each release
-  version "0.0.1"
+  version "0.0.2"
   url "https://github.com/muchbetteradventures/markie/releases/download/v#{version}/markie-#{version}.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "e83d812113ec67e5c16d35bf0de0503996cd65d42a8c1e88657d6d58f0ba765e"
 
   depends_on :macos
   depends_on macos: :sonoma
