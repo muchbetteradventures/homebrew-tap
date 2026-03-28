b040e9438e1a8a51e09a5a0d7597fae71333da6b
Andy Bennett <andy@muchbetteradventures.com>
2026-03-28 17:23:38 +0000

    marka 0.9.0

--- Casks/marka.rb
+++ Casks/marka.rb
@@ -1,6 +1,6 @@
 cask "marka" do
-  version "0.8.1"
-  sha256 "3a12163270b6654918223c47a61ad6228534c2243517358bce1861e61a401aa0"
+  version "0.9.0"
+  sha256 "a3781557a56adc7611b0643d914895ac4deabdbe1e097d31d97ef428dd8e3744"
 
   url "https://github.com/muchbetteradventures/marka/releases/download/v#{version}/marka-#{version}.dmg"
   name "Marka"
